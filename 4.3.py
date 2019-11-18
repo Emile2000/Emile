e7eeaa0f4841417996ed992016efa989051338d7
Emile <boutryemile1@gmail.com>
2019-11-18 11:53:43 +0100

    yo

---
+++ 4.3.py
@@ -0,0 +1,6 @@
+N = int(input("Entrez un nb"))
+print(N)
+nMax = N +10
+while N < nMax:
+    N = N+1
+    print("Le nb suivant est" , N)
---
+++ 4.5.py
@@ -0,0 +1,5 @@
+N = int(input("Entrez un nb"))
+i=0
+while i < 10:
+    print(N,"*",i+1,"=", N* (i+1))
+    i += 1
